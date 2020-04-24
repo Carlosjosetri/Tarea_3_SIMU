2bee8300930c6bda11214d177280b57bfebc0823
carlosjosetri <00082217@uca.edu.sv>
2020-04-24 13:22:06 -0600

    initial commit

---
+++ cmake-build-debug/CMakeFiles/_TSC_Laboratorio3_master.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_TSC_Laboratorio3_master.dir/main.cpp.obj"
+  "_TSC_Laboratorio3_master.pdb"
+  "_TSC_Laboratorio3_master.exe"
+  "_TSC_Laboratorio3_master.exe.manifest"
+  "lib_TSC_Laboratorio3_master.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/_TSC_Laboratorio3_master.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
